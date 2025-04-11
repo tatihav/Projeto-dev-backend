8c2243e52a0ddda5e04323caa38bc4af222959c5
Tatiana Haveroth Barbosa <haverothtatiana@gmail.com>
2025-04-11 11:31:44 -0300

    primeiro commit com .gitignore

---
+++ dev_backend/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dev_backend.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ dev_backend/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dev_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório inserir Nome!")]
+        public string Nome { get; set; } 
+
+        [Required(ErrorMessage = "Obrigatório inserir Placa!")]
+        public string Placa { get; set; } 
+
+        [Required(ErrorMessage = "Obrigatório inserir Ano de Fabricação!")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório inserir Ano de Modelo!")]
+        public int AnoModelo { get; set; }
+    }
+}
